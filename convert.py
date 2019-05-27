2e6539ae50042a7677c3da10d8af19321d73a041
Zohar Sacks <zohar.sacks@iqoqo.co>
2019-05-27 13:54:07 +0300

    first working version

--- convert.py
+++ convert.py
@@ -3,10 +3,11 @@ import time
 import os
 
 FILE_PATH = "sample_data/rssdemo.xml"
-OUT_DIR= "out\\"
+OUT_DIR= "out/{0}/"
 
 def dir_from_date(path):
 	try:
+		print(path)
 		os.makedirs(path)
 	except OSError:
 		print ("Creation of the directory %s failed" % path)
@@ -25,9 +26,15 @@ def make_front_metter(item2, formated):
 	#     - stuff
 	#     - other
 	# ---
+
+	episode_image = 'https://pbcdn1.podbean.com/imglogo/image-logo/1676116/r_My_Post_3_.jpg'
+	if 'itunes:image' in item.keys():
+		episode_image = item['itunes:image']['@href']
+		
+
 	return "---\ntitle: \"{0}\"\ncover: \"{1}\"\nauthor: \"guinevere\"\ndate: \"{2}\"\n---".format(
 		item['title'],
-		item['itunes:image']['@href'],
+		episode_image,
 		formated)
 
 
@@ -36,18 +43,21 @@ with open(FILE_PATH,'r',50,"utf8") as fd13:
 	doc = xmltodict.parse(fd13.read())
 	items = doc['rss']['channel']['item']
 	for item in items:
-		creationDate = time.strptime(item['pubDate'],'%a, %d %b %Y %H:%M:%S +0300')
+		creation_date = time.strptime(item['pubDate'],'%a, %d %b %Y %H:%M:%S +0300')
 
 		# Post folder
-		formated = "{0}-{1:02d}-{2:02d}".format(creationDate.tm_year, creationDate.tm_mon, creationDate.tm_mday)
-		dir_from_date("{1}{0}".format(formated, OUT_DIR))
+		formated = '{0}-{1:02d}-{2:02d}'.format(creation_date.tm_year, creation_date.tm_mon, creation_date.tm_mday)
+		full_path = OUT_DIR.format(formated)
+		dir_from_date(full_path)
 
 		# front metter
 		front_metter = make_front_metter(item, formated)
-
-		file = open(“index.md”,”w”) 
 		
+		file = open('{0}/{1}'.format(full_path, 'index.md'),'w+') 
 		file.write(front_metter) 
-		file.write(“\n”) 
-		file.write(item["description"]) 
+		file.write('\n') 
+		if item["description"] is None:
+			file.write(item["title"]) 
+		else:
+			file.write(item["description"]) 
 		file.close() 
